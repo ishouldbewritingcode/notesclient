7ae7e9d82863ecffe125e46e7d4cfac9301b5d76
Bryan <ishouldbewritingcode@gmail.com>
2026-05-03 23:49:48 -0500

    Removed password for this simple demo app.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -53,7 +53,7 @@ export function Login() {
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <h2 className="mt-6 text-center text-3xl font-medium text-gray-900">
             Sign in to your account
           </h2>
         </div>
