a931dac1afbf724f57e2fd9b138ef60b2b8ac2b3
Bryan Mickelson <ishouldbewritingcode@hotmail.com>
2026-05-06 10:43:55 -0500

    Added a simple about page.

---
+++ src/routes/about.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/about')({ component: About })
+
+function About() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">About This App</h1>
+      <p className="mt-4 text-lg">This is a simple about page for the app.</p>
+    </div>
+  )
+}
