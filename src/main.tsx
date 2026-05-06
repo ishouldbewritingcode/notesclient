21419588ed918e514bacb1c1054453ec937f87e5
Bryan <ishouldbewritingcode@gmail.com>
2026-05-06 09:34:42 -0500

    Trying to get them to auth.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+
 
 const router = createRouter({
   routeTree,
@@ -15,8 +16,9 @@ declare module '@tanstack/react-router' {
 }
 
 const rootElement = document.getElementById('app')!
-
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+  root.render(
+    <RouterProvider router={router} />
+  )
 }
